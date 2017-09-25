7251cba4b171041d42576f9b4209d0fe7c5d494c
Justin Peacock <mrdink@users.noreply.github.com>
2017-09-25 14:32:30 -0400

    added apps

--- webdev.sh
+++ webdev.sh
@@ -46,3 +46,18 @@ phpcs --config-set installed_paths ~/wpcs
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
+
+# Install applications
+brew cask install atom
+brew cask install droplr
+brew cask install harvest
+brew cask install iterm2
+brew cask install private-internet-access
+brew cask install resilio-sync
+brew cask install sequel-pro
+brew cask install sip
+brew cask install sonos
+brew cask install transmit
+brew cask install tower
+brew cask install the-unarchiver
+brew cask install virtualbox
