3065bbfc88464e2ddd9d242498bd3532f8811544
Justin Peacock <mrdink@users.noreply.github.com>
2018-12-15 10:10:33 -0500

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -14,4 +14,19 @@ brew update
 brew upgrade -y
 
 # Install applications
-brew cask install atom alfred stat-menus the-unarchiver sourcetree iterm2 sip slack sketch sequel-pro transmit zoomus
+brew cask install atom
+brew cask install alfred
+brew cask install the-unarchiver
+brew cask install sourcetree
+brew cask install iterm2
+brew cask install sip
+brew cask install slack
+brew cask install sequel-pro
+brew cask install transmit
+brew cask install zoomus
+brew cask install microsoft-teams
+brew cask install resilio-sync
+brew cask install droplr
+brew cask install sonos
+brew cask install balenaetcher
+brew cask install discord
