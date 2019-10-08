518692d212e1de6987dd84dc1d18e0cb49e2b0b9
Justin Peacock <mrdink@users.noreply.github.com>
2019-10-08 19:58:45 -0400

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -57,8 +57,5 @@ brew install git-extras
 brew install speedtest_cli
 brew install ssh-copy-id
 
-# Install developer friendly quick look plugins; see https://github.com/sindresorhus/quick-look-plugins
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv qlimagesize webpquicklook suspicious-package
-
 # Remove outdated versions from the cellar.
 brew cleanup
